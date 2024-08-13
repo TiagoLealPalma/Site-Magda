04b3596becbc7af2f280061f9da0945a8fe43ea8
TiagoLealPalma <tiagolealpalma@gmail.com>
2024-08-13 04:02:18 +0100

    13/08

--- SiteMagda/static/scripts/landingpage.js
+++ SiteMagda/static/scripts/landingpage.js
@@ -1,15 +1,16 @@
-const headerSVG = document.getElementById('headerbackground');
+const headerSVG = document.getElementById('rectangle');
 
 window.addEventListener('scroll', () => {
 const scrollPosition = document.documentElement.scrollTop;
+const opacity = Math.max(0, Math.min(1, (scrollPosition/800))).toString()
 console.log(scrollPosition);
 
 
-    if (scrollPosition > 5 ){
+    if (scrollPosition > 800 ){
         headerSVG.style.opacity = '1';
 
     }else{
-        headerSVG.style.opacity = '0.35';
+        headerSVG.style.opacity = opacity;
     }
 
 });
