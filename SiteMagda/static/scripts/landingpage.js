d605d623dbd61c931011ebcb044a3835abd5864e
TiagoLealPalma <tiagolealpalma@gmail.com>
2024-08-13 17:43:22 +0100

    13/08(2)

--- SiteMagda/static/scripts/landingpage.js
+++ SiteMagda/static/scripts/landingpage.js
@@ -1,16 +1,27 @@
 const headerSVG = document.getElementById('rectangle');
 
 window.addEventListener('scroll', () => {
-const scrollPosition = document.documentElement.scrollTop;
-const opacity = Math.max(0, Math.min(1, (scrollPosition/800))).toString()
-console.log(scrollPosition);
 
+    /* Fade Header Bar */
+    const scrollPosition = document.documentElement.scrollTop;
+    const opacity = Math.max(0, Math.min(1, (scrollPosition/800))).toString()
+    console.log(scrollPosition);
 
     if (scrollPosition > 800 ){
         headerSVG.style.opacity = '1';
-
     }else{
         headerSVG.style.opacity = opacity;
     }
 
+    /* Fade in elements */
+    const elements = document.querySelectorAll('.hidden');
+    elements.forEach(element => {
+        const position = element.getBoundingClientRect();
+        if (position.top < window.innerHeight && position.bottom >= 0) {
+            element.classList.add('visible');
+        } else {
+            element.classList.remove('visible');
+        }
+    });
 });
+
