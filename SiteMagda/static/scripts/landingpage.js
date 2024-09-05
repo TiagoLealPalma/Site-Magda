150764b91273b28450d0c7940bb8cbea6c990be6
TiagoLealPalma <tiagolealpalma@gmail.com>
2024-09-05 19:41:50 +0100

    15/09
    lead section left and right side done

--- SiteMagda/static/scripts/landingpage.js
+++ SiteMagda/static/scripts/landingpage.js
@@ -3,24 +3,25 @@ let originalPos = true;
 let lastScroll = 0;
 
 window.addEventListener('scroll', () => {
-    /* Fade Header Bar */
+
     const viewheight = window.innerHeight - headerSVG.offsetHeight;
     const scrollPosition = document.documentElement.scrollTop;
     const opacity = Math.max(0, Math.min(1, (scrollPosition/viewheight))).toString();
-    const scrollDirection = lastScroll - scrollPosition;
 
-    if(scrollDirection > -1){
+    if(scrollPosition < lastScroll) {
+        // Header opacity change
         if (scrollPosition > viewheight) {
+
             headerSVG.style.backgroundColor = 'rgba(41,44,47,1)';
-            console.log("entrei 1")
-        }else {
+        } else {
             headerSVG.style.backgroundColor = 'rgba(41,44,47,' + opacity + ')';
-            console.log("entrei 0")
         }
+        // Scroll Down Animation Trigger
         headerSVG.style.opacity = '1';
     }else{
         headerSVG.style.opacity = '0';
     }
+
 
     lastScroll = scrollPosition;
 
@@ -36,12 +37,13 @@ window.addEventListener('scroll', () => {
     });
 });
 
-document.querySelectorAll('.clicable').forEach(item => {item.addEventListener('click', function (){
+
+/* .lead-section Animation */
+document.querySelectorAll('.clickable').forEach(item => {item.addEventListener('click', function (){
     const movableDiv = document.querySelector('.onTop');
     if (originalPos){
         movableDiv.style.transition = 'all ease-in-out 1s'
         movableDiv.style.transform = 'translate(100%)'
-        console.log("entrei")
     }else{
         movableDiv.style.transform = 'translate(0)'
     }
