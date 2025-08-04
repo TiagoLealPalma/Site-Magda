72498f467f048805419be5f6a3f0fd8631cd02a2
TiagoLealPalma <tiagolealpalma@gmail.com>
2025-08-04 11:37:24 +0100

    Media Queries tweaked for forsale page

--- SiteMagda/SiteMada/urls.py
+++ SiteMagda/SiteMada/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 """
 URL configuration for SiteMada project.
 
@@ -22,3 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- SiteMagda/static/scripts/forsale.js
+++ SiteMagda/static/scripts/forsale.js
@@ -7,3 +7,20 @@ menuToggle.addEventListener("click", () => {
     body.classList.toggle("menu-open");
     menuToggle.classList.toggle("slide")
 });
+
+window.addEventListener('scroll', function () {
+    const searchBarBackground = document.getElementById('search-bar-background');
+    const searchBar = document.getElementById('menu-toggle');
+    console.log("1")
+
+    if (window.scrollY > 10) {
+        console.log("2")
+        searchBarBackground.classList.add('scrolled-header');
+        searchBar.classList.add('scrolled-helper');
+
+    } else {
+        console.log("3")
+        searchBarBackground.classList.remove('scrolled-header');
+        searchBar.classList.remove('scrolled-helper');
+    }
+});
